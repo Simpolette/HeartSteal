c6dd519d5fabb9feee9709aeb468217aba5e227a
Simpolette <84978940+Simpolette@users.noreply.github.com>
2026-01-21 16:50:27 +0700

    [BE] Add cors for web migration

--- server/internal/route/route.go
+++ server/internal/route/route.go
@@ -8,14 +8,26 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func Setup(env *bootstrap.Env, timeout time.Duration, db *mongo.Database, gin *gin.Engine) {
+	gin.SetTrustedProxies(nil)
+
+	gin.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	
 	publicRouter := gin.Group("/api")
 	// All Public APIs
 	NewUserRouter(env, timeout, db, publicRouter)
 
-	protectedRouter := gin.Group("")
+	protectedRouter := gin.Group("/api")
 	protectedRouter.Use(middleware.JwtAuthMiddleware(env.AccessTokenSecret))
 	// All Private APIs
 }
