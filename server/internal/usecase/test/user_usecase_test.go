74ea4d4a9f6363f6ec915fe6f21226508ad5010b
Simpolette <84978940+Simpolette@users.noreply.github.com>
2026-01-24 16:47:56 +0700

    [CI/CD] Test test successful

--- server/internal/usecase/test/user_usecase_test.go
+++ server/internal/usecase/test/user_usecase_test.go
@@ -148,6 +148,6 @@ func Test_Login(t *testing.T) {
 
 		assert.Error(t, err)
 		assert.Empty(t, token)
-		assert.Equal(t, domain.ErrUserNotFound, err)
+		assert.Equal(t, domain.ErrInvalidCredentials, err)
 	})
 }
